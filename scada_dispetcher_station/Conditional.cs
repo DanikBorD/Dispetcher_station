36de93fa1e7af305df2ccda5232273d9709c99b8
borisenko <d.borisenko@wellsoft.pro>
2017-05-12 18:08:57 +0500

    1

--- scada_dispetcher_station/Conditional.cs
+++ scada_dispetcher_station/Conditional.cs
@@ -15,8 +15,8 @@ namespace scada_dispetcher_station
 {
     class Conditional
     {
+        ToggleButton btn = FindViewById<ToggleButton>(Resource.Layout.ConditionalToggleButton);
         Connect cnn;
-        public int NumberOfFlap;
         public UInt16 OnOff;
         public UInt16 SetTemperature = Convert.ToUInt16(1);
         public UInt16 readConditional;
@@ -48,8 +48,16 @@ namespace scada_dispetcher_station
 
             //result = true;
             //return result;
-
+            if(OnOff == 1)
+            {
+                btn.Activated = true;
+            }
+            else
+            {
+                btn.Activated = false;
+            }
             return true;
+            
         }
 
 
@@ -84,11 +92,7 @@ namespace scada_dispetcher_station
             //UInt16 result = 0;
             //UInt16 dm_position = 100;
 
-            //if(NumberOfFlap == 2)
-            //{
-            //    dm_position = 102;
-            //}
-            //readFlap = 0;
+            //readConditional = 0;
             //bool in_2_0 = false;
             //bool in_2_1 = false;
             //bool in_2_2 = false;
@@ -96,27 +100,17 @@ namespace scada_dispetcher_station
 
             //try
             //{
-            //    result = cnn.plc.ReadDM(dm_position, ref statusFlap);
-            //    if (!cnn.plc.ReadDM(dm_position, ref statusFlap))
+            //    result = cnn.plc.ReadDM(dm_position, ref statusConditional);
+            //    if (!cnn.plc.ReadDM(dm_position, ref statusConditional))
             //    {
             //        throw new Exception(this.cnc.plc.LastError);
             //    }
 
-            //    if ((Convert.ToUInt16(statusFlap) & Convert.ToUInt16(1)) == Convert.ToUInt16(1)) in_2_0 = true;
-            //    if ((Convert.ToUInt16(statusFlap) & Convert.ToUInt16(2)) == Convert.ToUInt16(2)) in_2_1 = true;
-            //    if ((Convert.ToUInt16(statusFlap) & Convert.ToUInt16(4)) == Convert.ToUInt16(4)) in_2_2 = true;
-            //    if ((Convert.ToUInt16(statusFlap) & Convert.ToUInt16(8)) == Convert.ToUInt16(8)) in_2_3 = true;
+            //    if ((Convert.ToUInt16(statusConditional) & Convert.ToUInt16(1)) == Convert.ToUInt16(1)) in_2_0 = true;
+            //    if ((Convert.ToUInt16(statusConditional) & Convert.ToUInt16(2)) == Convert.ToUInt16(2)) in_2_1 = true;
+            //    if ((Convert.ToUInt16(statusConditional) & Convert.ToUInt16(4)) == Convert.ToUInt16(4)) in_2_2 = true;
+            //    if ((Convert.ToUInt16(statusConditional) & Convert.ToUInt16(8)) == Convert.ToUInt16(8)) in_2_3 = true;
 
-
-            //    if (in_2_0)
-            //    {
-            //        string status = "Полностью открыто";
-            //    }
-
-            //    if (in_2_1)
-            //    {
-            //        string status = "Полностью закрыто";
-            //    }
             //}
             //catch (Exception ex)
             //{
